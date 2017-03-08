52b4767b8ee85c8e26d3757c0c8bd64f1d73f4aa
Alexis AGGERY <alexis.aggery@gmail.com>
2017-03-08 04:35:00 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -11,14 +11,21 @@ from PIL import Image
 debugging = True
 running = True
 
-bg_filter = cv2.createBackgroundSubtractorMOG2( detectShadows=True )
+people_list = np.array( [  ] )
+people_in_bus = 0
+img_w = 1280
+img_h = 720
 
 cam = picamera.PiCamera()
-cam.resolution = (1280, 720)
-RAWcapture = PiRGBArray( cam, size=( 1280, 720 ) )
+cam.resolution = (img_w, img_h)
+cam.hflip = False
+RAWcapture = PiRGBArray( cam, size=( img_w, img_h ) )
 
 
 
+# defining fonctions
+def update_output( number ) :
+	print(" people in the bus : %s", number )
 
 def get_img() :
 	cam.capture(RAWcapture, format="rgb")
@@ -49,27 +56,69 @@ def detect_frontface( img ) :
 def detect_profileface( img ) :
 	return load_detected('haarcascade_profileface.xml', img)
 
-
-#while ( running ) :
-current_img = get_img()
-
-if (debugging) :
-	debug_img_show( "raw rgb image", current_img )
-
-frontfaces_detected = detect_frontface( current_img )
-profilefaces_detected = detect_profileface( current_img )
-
-print( len( frontfaces_detected ) + len( profilefaces_detected ) )
-
-if (debugging) :
-	for (x, y, w, h) in frontfaces_detected :
-		print("x: %s   y: %s ", x, y)
-		cv2.rectangle( current_img, (x, y), (x + w, y + h), (0, 255, 0), 2 )
+def set_zone( people_list) :
+	for i in range(0, len( people_list ) ) :
+		if ( people_list[ i ]['zone'] != 'N' ) :
+			if ( people_list[ i ]['zone'] == 'B' and people_list[ i ]['x'] > ( img_w / 2 ) ) :
+				people_in_bus = people_in_bus + 1
+				# suppr array case
+				index = np.argwhere( people_list == i )
+				people_list = np.delete( people_list, index )
+			elif ( people_list[ i ]['zone'] == 'R' and people_list[ i ]['x'] < ( img_w / 2 ) ) :
+				people_in_bus = people_in_bus - 1
+				# suppr array case
+				index = np.argwhere( people_list == i )
+				people_list = np.delete( people_list, index )
+			
+		
 	
-	for (x, y, w, h) in profilefaces_detected :
-		print("x: %s   y: %s ", x, y)
-		cv2.rectangle( current_img, (x, y), (x + w, y + h), (0, 255, 0), 2 )
+
+
+while ( running ) :
+	current_img = get_img()
+
+	if (debugging) :
+		cv2.rectangle( current_img, (0, 0), (img_w / 2, img_h), (255, 0, 0), 6 )
+		cv2.rectangle( current_img, (img_w / 2, 0), (img_w, img_h), (0, 0, 255), 6 )
+		debug_img_show( "raw rgb image", current_img )
+
+	frontfaces_detected = detect_frontface( current_img )
+	profilefaces_detected = detect_profileface( current_img )
+
+	print( len( frontfaces_detected ) + len( profilefaces_detected ) )
+
+	if (debugging) :
+		if ( len( frontfaces_detected ) != 0 ) :
+			for (x, y, w, h) in frontfaces_detected :
+				print("x: %s   y: %s ", x, y)
+				cv2.rectangle( current_img, (x, y), (x + w, y + h), (0, 255, 0), 2 )
 	
-	debug_img_show( "rects for fullbody", current_img )
+		if ( len( profilefaces_detected ) != 0 )  :
+			for (x, y, w, h) in profilefaces_detected :
+				print("x: %s   y: %s ", x, y)
+				cv2.rectangle( current_img, (x, y), (x + w, y + h), (0, 255, 0), 2 )
+	
+		debug_img_show( "rects for fullbody", current_img )
 
 
+
+	people_count = 0
+	if ( len( frontfaces_detected ) != 0 ) :
+		for (x, y, w, h) in frontfaces_detected :
+			people_list = np.append( people_list, { 'x':x, 'y':y, 'zone':'N' } )
+			if (debugging) :
+				print people_list
+				cv2.waitKey(0)
+			people_count = people_count + 1
+
+	if ( len( profilefaces_detected ) != 0 ) :
+		for (x, y, w, h) in profilefaces_detected :
+			people_list = np.append(people_list, { 'x':x, 'y':y, 'zone':'N' } )
+			if (debugging) :
+				print people_list
+				cv2.waitKey(0)
+			people_count = people_count + 1
+
+	set_zone( people_list )
+	update_output( people_in_bus )
+# redo
